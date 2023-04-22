b0bb8ba4f25e23595ff8435399fc156a32dec1f2
MinhTien999 <minhtien161002@gmail.com>
2023-04-22 17:43:23 +0700

    update chat

--- script.js
+++ script.js
@@ -4,12 +4,44 @@ const messagesContainer = document.querySelector('#messages');
 
 let isUser = true;
 
+// Tạo WebSocket object
+const socket = new WebSocket('ws://localhost:8080');
+
+// Xử lý sự kiện khi mở kết nối WebSocket
+socket.addEventListener('open', event => {
+  console.log('WebSocket connected');
+});
+
+// Xử lý sự kiện nhận tin nhắn từ server
+socket.addEventListener('message', event => {
+  const message = event.data;
+  const messageContainer = document.createElement('div');
+  const messageText = document.createElement('span');
+
+  messageText.textContent = message;
+  messageContainer.appendChild(messageText);
+  messagesContainer.appendChild(messageContainer);
+
+  messageContainer.classList.add('message-container');
+
+  if (isUser) {
+    messageContainer.classList.add('message-container--user');
+  } else {
+    messageContainer.classList.add('message-container--bot');
+  }
+
+  isUser = !isUser;
+});
+
 // Xử lý sự kiện gửi tin nhắn
 function sendMessage(event) {
   event.preventDefault();
   const message = messageInput.value;
   if (message.trim() === '') return;
-  
+
+  // Gửi tin nhắn lên server qua WebSocket
+  socket.send(message);
+
   if (message.trim().toLowerCase() === 'delete all') {
     messagesContainer.innerHTML = '';
     localStorage.removeItem('messages');
@@ -35,7 +67,7 @@ function sendMessage(event) {
 
   messageInput.value = '';
   messageInput.focus();
-  
+
   // Lưu tin nhắn vào localStorage
   const messages = JSON.parse(localStorage.getItem('messages')) || [];
   messages.push(message);
