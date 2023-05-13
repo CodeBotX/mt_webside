0663020fcdd52445a2c74bc10affc6c6b5521229
MinhTien999 <minhtien161002@gmail.com>
2023-05-13 18:00:32 +0700

    update 13/5

--- script.js
+++ script.js
@@ -22,4 +22,42 @@ function handleLogin() {
   });
 }
 
+function startTest() {
+  document.getElementById("test").style.display = "block";
+  var minutes = 0; // Thời gian kiểm tra
+  var seconds = 5;
+  var countdown = setInterval(function() {
+    if (minutes == 0 && seconds == 0) {
+      clearInterval(countdown);
+      countdown = null;
+      document.getElementById("test").style.display = "none";
+      document.getElementById("timesUp").style.display = "block";
+      document.getElementById("start-button").style.display = "none";
+      document.getElementById("tieu-de").style.display = "none";
+    } else {
+      if (seconds == 0) {
+        minutes--;
+        seconds = 59;
+      } else {
+        seconds--;
+      }
+      document.getElementById("minutes").textContent = minutes;
+      document.getElementById("seconds").textContent = seconds;
+    }
+  }, 1000);
+}
+
+
+var currentDate = document.getElementById("current-date");
+
+// Lấy ngày tháng hiện tại
+var now = new Date();
+var day = now.getDate();
+var month = now.getMonth() + 1;
+var year = now.getFullYear();
+
+// Gán ngày tháng hiện tại vào nội dung của thẻ span
+currentDate.innerHTML = day + "/" + month + "/" + year;
+
+
 handleLogin();
